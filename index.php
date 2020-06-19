b827e27e4d02a3f928bb5358cd38b50c35f26ed5
arjunkr123 <39012055+arjunkr123@users.noreply.github.com>
2020-06-19 19:37:51 +0530

    dgrg

--- index.php
+++ index.php
@@ -46,17 +46,19 @@ if(isset($_POST["submit"]))
 	';
 
 	require 'class/class.phpmailer.php';
+	require 'class/class.exception.php';
+require 'class/class.smtp.php';
 	$mail = new PHPMailer;
 	$mail->IsSMTP();								//Sets Mailer to send message using SMTP
 	$mail->Host = 'smtp.gmail.com';		//Sets the SMTP hosts of your Email hosting, this for Godaddy
 	$mail->Port = '587';								//Sets the default SMTP server port
 	$mail->SMTPAuth = true;							//Sets SMTP authentication. Utilizes the Username and Password variables
-	$mail->Username = 'tarunkrdev10@gmail.com';					//Sets SMTP username
-	$mail->Password = 'reward23';					//Sets SMTP password
+	$mail->Username = 'apnidawaa19@gmail.com';					//Sets SMTP username
+	$mail->Password = 'apnidawaavaranasi';					//Sets SMTP password
 	$mail->SMTPSecure = 'tls';							//Sets connection prefix. Options are "", "ssl" or "tls"
 	$mail->From = $_POST["email"];					//Sets the From email address for the message
 	$mail->FromName = $_POST["name"];				//Sets the From name of the message
-	$mail->AddAddress('tarunkrdev10@gmail.com', 'Gmail');		//Adds a "To" address
+	$mail->AddAddress('apnidawaa19@gmail.com', 'Gmail');		//Adds a "To" address
 	$mail->WordWrap = 50;							//Sets word wrapping on the body of the message to a given number of characters
 	$mail->IsHTML(true);							//Sets message type to HTML
 	$mail->AddAttachment($path);					//Adds an attachment from a path on the filesystem
@@ -220,7 +222,7 @@ if(isset($_POST["submit"]))
 										<span class="info-icon"><i class="fa fa-ambulance" aria-hidden="true"></i></span>
 										<h3>Delivery service</h3>
 
-										<p> Now, you can order your medicines online through websites , You can also avail cash on delivery service across locations in Prayagraj and Varanasi, the minimum order is Rs 10.
+										<p> Now, you can order your medicines online through websites , You can also avail cash on delivery service across locations in  Varanasi, the minimum order is Rs 10.
 								 </div>
 							</div>
 					 </div>
@@ -296,7 +298,7 @@ if(isset($_POST["submit"]))
 					<div class="form-group" align="center">
 						<input type="submit" name="submit" value="Submit" class="btn btn-info" />
 					</div>
-			<p> if you don't have prescription you can order through Whatsapp</P>
+			<p> If you don't have prescription you can order through Whatsapp</P>
 				</div>
 
 			</div>
@@ -312,6 +314,7 @@ if(isset($_POST["submit"]))
 										 <div class="myDiv">
 											<h2>You CAN PAY THROUGH UPI</h2>
 											<p>apnidawaa@upi</p>
+               click here<a href="upi://pay?pa=apnidawaa@upi" data-scroll="" class="btn btn-light btn-radius btn-brd grd1 effect-1">Pay through paytm</a>
 										</div>
 
 						<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
@@ -385,11 +388,18 @@ if(isset($_POST["submit"]))
 								 <div class="message-box">
 										<h4>What We Do</h4>
 										<h2>Online Medicine Delivery Service</h2>
-										<p class="lead">Apni Dawaa is an online medicine delivery website, which  allows you to buy healthcare products, OTC products and medical equipment online. With website, you can also book diagnostic tests,   from the convenience of your home.
+										<p class="lead">Apni Dawaa is an online medicine delivery website, which  allows you to buy healthcare products, OTC products and medical equipment online. With website, from the convenience of your home.
 
 </p>
 										<p> Our partner retailer will call you to confirm the healthcare products you need along with their quantities . </p>
-										<a href="#services" data-scroll class="btn btn-light btn-radius btn-brd grd1 effect-1">Learn More</a>
+
+
+										<div class="columns download">
+          <p>
+             	<a href="images/Apnidawaaprivacypolicy.pdf" data-scroll class="btn btn-light btn-radius btn-brd grd1 effect-1">Terms and Conditions</a>
+          </p>
+       </div>
+
 								 </div>
 								 <!-- end messagebox -->
 							</div>
@@ -477,7 +487,7 @@ if(isset($_POST["submit"]))
 
 
                                  </div>
-																 The ongoing demand of our customer’s belief. Himalaya and Patanjali are the brand new companies with their customized products. Apart from this there are many other ayurved medicines
+																 The ongoing demand of our customer’s belief.  brand new companies with their customized products. Apart from this there are many other ayurved medicines
 
                                  <div class="pricing-table-sign-up">
                                     <a href="upi://pay?pa=apnidawaa@upi" data-scroll="" class="btn btn-dark btn-radius btn-brd">Order Now</a>
@@ -491,7 +501,7 @@ if(isset($_POST["submit"]))
 															</div>
                                  <div class="pricing-table-space"></div>
 
-																 We provide Allopathy medicines which is a Leading &amp; trusted name among all pharma companies. We would like to introduce ourselves as a marketing company with wide range of Syrups, Capsules, Tablets, Injectibels, Ointments and many more.</p>
+																 We provide Allopathy medicines which is a Leading &amp; trusted name among all pharma companies. We would like to introduce ourselves as a marketing company with wide range of Syrups, Capsules, Tablets, injections, Ointments and many more.</p>
 
                                  <div class="pricing-table-sign-up">
                                     <a href="upi://pay?pa=apnidawaa@upi" data-scroll="" class="btn btn-light btn-radius btn-brd grd1 effect-1">Order Now</a>
@@ -589,7 +599,7 @@ if(isset($_POST["submit"]))
                <div class="col-md-4">
                   <div class="logo padding">
                      <a href=""><img src="images/logo.png" alt=""></a>
-                     <p>Buy medicines online from India's favorite e-pharmacy. Buy prescription medicines, OTC products & household needs. Cash on delivery. Home delivery.</p>
+                     <p>Buy medicines online from India's favorite medicine delivery service. Buy prescription medicines, OTC products & household needs. Cash on delivery. Home delivery.</p>
                   </div>
                </div>
                <div class="col-md-4">
